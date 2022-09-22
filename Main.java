e9aef84fd44cf6e4ee9a9f65d3547c83a6f59551
Omne Jean Pelingon <21103810@usc.edu.ph>
2022-09-22 20:14:33 +0800

    Omne Jean uploaded an initial java file

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      Person var = new Person("Omne"); // ang Omne nga value  didtu na siya ma assign sa var no need na angg setname 
+      
+      System.out.println(var.getName()); 
+    }
+
+}     
